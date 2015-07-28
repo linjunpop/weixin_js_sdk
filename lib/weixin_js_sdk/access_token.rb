217c07f13b4c226649c9b766018a2e2708a41bf0
tiroc <chaoyang.che@gmail.com>
2015-07-28 16:00:19 +0800

    Bump to v0.0.2
    * Deprecated `fetch`
    * Add `token` and `expires_in` to WeixinJsSDK::AccessToken
    * Add `token` and `expires_in` to WeixinJsSDK::Ticket

--- lib/weixin_js_sdk/access_token.rb
+++ lib/weixin_js_sdk/access_token.rb
@@ -2,23 +2,26 @@ module WeixinJsSDK
   class AccessToken
     URI_TEMPLATE = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%{app_id}&secret=%{app_secret}'.freeze
 
-    def initialize(app_id: '', app_secret: '')
-      @app_id = app_id
-      @app_secret = app_secret
-    end
+    attr_reader :token, :expires_in
 
-    def fetch
+    def initialize(app_id: '', app_secret: '')
       url = URI_TEMPLATE % {
-        app_id: @app_id,
-        app_secret: @app_secret
+        app_id: app_id,
+        app_secret: app_secret
       }
 
       json = Util.get_json(url)
 
-      access_token = json['access_token']
-      expires_in = json['expires_in']
-
-      access_token
+      @token = json['access_token']
+      @expires_in = json['expires_in']
     end
+
+    # <b>DEPRECATED:</b> Please use <tt>token</tt> instead.
+    def fetch
+      warn "[DEPRECATION] `fetch` is deprecated.  Please use `token` instead."
+      token
+    end
+
+    alias_method :access_token, :token
   end
 end
